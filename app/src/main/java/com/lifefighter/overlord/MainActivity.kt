3c776b2d44ee773c74b788b16789aff0474cd07e
xzp <xuezhenpu@xiaomai5.com>
2021-03-08 11:54:16 +0800

    dataBinding适配器优化

--- app/src/main/java/com/lifefighter/overlord/MainActivity.kt
+++ app/src/main/java/com/lifefighter/overlord/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.lifefighter.overlord
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import com.lifefighter.base.BaseActivity
+import com.lifefighter.overlord.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+class MainActivity : BaseActivity<ActivityMainBinding>() {
+    override fun getLayoutId(): Int = R.layout.activity_main
+    override fun onLifecycleStart() {
+        setSupportActionBar(viewBinding.toolbar)
+
     }
 }
---
+++ app/src/main/java/com/lifefighter/overlord/MainApplication.kt
@@ -0,0 +1,9 @@
+package com.lifefighter.overlord
+
+import android.app.Application
+
+/**
+ * @author xzp
+ * @created on 2021/3/8.
+ */
+class MainApplication : Application()
